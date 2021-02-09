999ae6e0a9c473805bfdd1ea60e97a2b326913ca
dhsj8405 <60701130+dhsj8405@users.noreply.github.com>
2021-02-09 23:26:47 +0900

    Add files via upload

--- todo.js
+++ todo.js
@@ -1,10 +1,28 @@
-const toDoForm = document.querySelector(".js-toDoForm"),
+  const toDoForm = document.querySelector(".js-toDoForm"),
 toDoInput = toDoForm.querySelector("input");
 toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS  = "toDos";
 
-const toDos = [];
+let toDos = [];
+
+
+function deleteToDo(event){
+  // console.dir(event.target.parentNode); //콘솔창에서 버튼의 fater를 찾을 수 있음 parent node
+  //-----------------html화면에서 toDo를 지우는 단계---------
+  //console.log(event.target.parentNode);
+  const btn = event.target
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  //-----------------localStorage에서 toDo 지우기------------------
+  const cleanToDos = toDos.filter(function(toDo){
+  // console.log(toDo.id, li.id);   //밑의 조건문이 실행이안돼서 두개의 데이터타입이 다름을 확인
+    return toDo.id !== parseInt(li.id); //string을 숫자로바꿈
+  }); //filter : array의 모든 아이템을 통해 함수를 실행
+      //필터는 forEach에서 function을 실행하는 것ㄱ 같이 각각의 item과 같이 실행
+  toDos=cleanToDos; //toDos가 const로 선언돼있었기때문에 let으로 바꿔줌
+  saveToDos();
+}
 
 function saveToDos(){
   localStorage.setItem(TODOS_LS,JSON.stringify(toDos));
@@ -18,7 +36,8 @@ function paintToDo(text){
   const delBtn = document.createElement("button");
   const span = document.createElement("span");
   const newId = toDos.length +1;
-  delBtn.innerText = "✖"
+  delBtn.innerText = "✖";
+  delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(delBtn);   //버튼을 li에 넣는것
   li.appendChild(span);    //뭔가를 그의 father element안에 넣는것
@@ -40,8 +59,8 @@ function handleSubmit(event){
 }
 
 function something (toDos){
-  console.log(toDos.id);
-  paintToDo(toDos.id)
+  console.log(toDos.text);
+  paintToDo(toDos.text)
 }
 
 function loadToDos(){
